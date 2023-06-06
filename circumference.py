e7127b18453e4c9978ddc2bd77dfae39b5f09dc9
JP Appel <jeanpierre.appel01@gmail.com>
2023-06-05 21:48:41 -0400

    local circumference parameter

---
+++ circumference.py
@@ -0,0 +1,21 @@
+import networkx as nx
+
+def circumference(g: nx.Graph) -> int:
+    """
+    Compute the cicumference of a graph, 0 if acyclic
+    """
+    cycles = list(nx.cycles.simple_cycles(g))
+    cycle_sizes = sorted(cycles, key = lambda s : len(s))
+    return len(cycle_sizes[-1]) if len(cycle_sizes) else 0
+
+def _has_l_circ(g: nx.Graph, l: int) -> bool:
+    """
+    Checks of a graph has at least circumference l
+    """
+    return circumference(g) >= l
+
+def has_l_circ(l: int) -> Callable:
+    """
+    Anonymous function generator for _has_l_circ
+    """
+    return lambda g : has_l_circ(g, l)
